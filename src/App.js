9d112c56bb2b4cf6155aba70ea385235bd8f99db
hamed zabihi <hamed0zabihi@gmail.com>
2021-02-26 22:45:28 +0330

    level one

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React from "react";
+import  Person from "./component/person";
 
 
 
 
 class App extends React.Component{
+constructor(){
+  super();
+  this.state={
+     data:[
+          {name:"hamed",family:"zabihi",city:"teh"},
+          {name:"obeyid",family:"zakani",city:"ghazvin"},
+          {name:"saadi",family:"shirazi",city:"shiraz"}
+        ]
+};
+}
+
+
 
   render(){
-
-    const state={};
+ 
+    const persoe=this.state.data;
+    console.log("1"+persoe);
     return(
+    
       <div>
         <h1>hi </h1>
+        <Person persons={persoe}/>
       </div>
     );
   }
